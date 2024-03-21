0f2556152f1f0c414c8356a0a66de178210318ff
Héctor José Díaz Sandate <hectorjosediazsandate@gmail.com>
2024-03-21 01:02:26 -0600

    Archivos de MOngoAtlas 2

--- src/config/db.js
+++ src/config/db.js
@@ -3,13 +3,7 @@ const MONGO_URL = "mongodb+srv://hectorjosediazsandate:hector0303@cluster0.e2xbk
 
 export const connectDB = async () => {
   try {
-    console.log("Waiting for MongoDB to start...");
-    await new Promise(resolve => setTimeout(resolve, 5000)); // Delay for 5 seconds
-    console.log("Connecting to DB...");
-    await mongoose.connect(MONGO_URL, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    });
+    await mongoose.connect(MONGO_URL);
     console.log("Connected to DB");
   } catch (error) {
     console.log("Error connecting to DB:", error);
